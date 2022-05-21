9a833b6cc6ea668f19685008fc515f037b40ea60
Raman Hira <ramanhira@gmail.com>
2022-05-21 09:48:13 +0530

    Turtle Graphics

---
+++ Turtle Graphics Practice 2.py
@@ -0,0 +1,21 @@
+# draw a ninja
+import _tkinter
+import turtle
+ninja = turtle.Turtle()
+ninja.speed(10)
+
+for i in range(180):
+    ninja.forward(100)
+    ninja.right(30)
+    ninja.forward(20)
+    ninja.left(60)
+    ninja.forward(50)
+    ninja.right(30)
+
+    ninja.penup()
+    ninja.setPosition(0, 0)
+    ninja.pendown()
+
+    ninja.right(2)
+
+turtle.done()
